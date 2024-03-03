10053928448ea776ebe9773d8a2f667af2e396dc
baldwisj <baldwisj@gmail.com>
2024-03-03 13:41:24 -0500

    Created Nav bar and set up routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 // Bringing in the required import from 'react-router-dom'
 import { Outlet } from 'react-router-dom';
-
+import './App.css'
 import Header from './components/Header';
-import Footer from '/componenter/Footer';
+import Footer from './components/Footer';
 
 function App() {
   // The Outlet component will conditionally swap between the different pages according to the URL
@@ -12,7 +12,7 @@ function App() {
       <Outlet />
       <Footer />
     </>
-  );
+  )
 }
 
 export default App;
